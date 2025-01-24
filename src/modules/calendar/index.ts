9779763e43022fcf68b75c88c23a354dd2f004f8
Rodrigo Girão <rodrigogirao7@gmail.com>
2025-01-24 15:40:07 -0300

    feat: asking for calendar permission

--- src/modules/calendar/index.ts
+++ src/modules/calendar/index.ts
@@ -1,9 +1,27 @@
-import {NativeModules} from 'react-native';
+import {Alert, NativeModules, PermissionsAndroid} from 'react-native';
 
 const {CalendarModule} = NativeModules;
 
-function createEvent(name: string, description: string) {
-  CalendarModule.createCalendarEvent(name, description);
+async function createEvent(name: string, description: string) {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.WRITE_CALENDAR,
+      {
+        title: 'Calendar Permission',
+        message: 'This app needs calendar access to add events',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (granted !== PermissionsAndroid.RESULTS.GRANTED) {
+      Alert.alert('Permission denied');
+      return;
+    }
+
+    CalendarModule.createCalendarEvent(name, description);
+  } catch (error) {
+    Alert.alert(`Error adding reminder to calendar: ${error}`);
+  }
 }
 
 export const Calendar = {
